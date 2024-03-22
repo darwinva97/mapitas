f0615f44783a930fe17d6eb49e9aecb89e064aa2
Darwin Stalin Vilcachagua Ayala <darwin97.va@gmail.com>
2024-03-21 22:12:57 -0500

    responsive

--- src/MapProvinces/components/ShowNames/index.tsx
+++ src/MapProvinces/components/ShowNames/index.tsx
@@ -11,22 +11,29 @@ export const ShowNames = ({
   id: string;
 } & HTMLAttributes<HTMLDivElement>) => {
   useEffect(() => {
-    const wrapper = document.querySelector(`[data-wrapper-map-id="${id}"]`);
-    wrapper?.querySelectorAll("path").forEach((path) => {
-      const id = path.getAttribute("id")!;
-      const isAvailable = availableProvinces.find((p) => p.id === id)!!;
+    const fn = () => {
+      const wrapper = document.querySelector(`[data-wrapper-map-id="${id}"]`);
+      wrapper?.querySelectorAll("path").forEach((path) => {
+        const id = path.getAttribute("id")!;
+        const isAvailable = availableProvinces.find((p) => p.id === id)!!;
 
-      if (!isAvailable) return;
+        if (!isAvailable) return;
 
-      const { top, left } = path.getBoundingClientRect();
+        const { top, left } = path.getBoundingClientRect();
 
-      const nameEl = wrapper.querySelector(`[data-map-province-id="${id}"]`);
+        const nameEl = wrapper.querySelector(`[data-map-province-id="${id}"]`);
 
-      if (!nameEl || !(nameEl instanceof HTMLDivElement)) return;
+        if (!nameEl || !(nameEl instanceof HTMLDivElement)) return;
 
-      nameEl.style.top = `${top}px`;
-      nameEl.style.left = `${left}px`;
-    });
+        nameEl.style.top = `${top}px`;
+        nameEl.style.left = `${left}px`;
+      });
+    };
+    fn();
+    window.addEventListener("resize", fn);
+    return () => {
+      window.removeEventListener("resize", fn);
+    };
   }, []);
   return (
     <>
